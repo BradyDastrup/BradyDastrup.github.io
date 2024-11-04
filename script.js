ae9e55cad88025c698b34a615142bd6b2c17ebf0
BradyDastrup <Bdastrup@hotmail.com>
2024-11-04 15:40:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var count=1
 
 function select(input) {
-
-if (count % 2 === 0)
+  if (count < 10) {
+    if (count % 2 === 0) {
+      document.getElementById(input.id).className = 'used1'
+  }
+  else {
+    document.getElementById(input.id).className = 'used2'
+  }
+  count+=1
+  }
 }
